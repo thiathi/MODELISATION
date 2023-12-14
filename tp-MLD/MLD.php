c16f82cb39701d372f69e346d2cdd4638dd7cb6c
MathieuQtrd <amirathiamy@gmail.com>
2023-12-14 15:44:32 +0100

    add tp mld

---
+++ tp-MLD/MLD.php
@@ -0,0 +1,8 @@
+<?php
+
+//-->commande 1, N (1 user pour N order)
+user (id, email, password, firstname , lastname, birthdate, adress, complements, postal_code, city, #id_order)
+
+// --> contient, 1, N (1 order pour N order_line)
+order (id, reference, date,)
+#order_line_id)
